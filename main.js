ef282a185c257020e3dd2a47e7b5e2ea762d902d
alvaropfn <alvaropfn@gmail.com>
2017-04-15 08:48:44 -0300

    refix first working socket emit state

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-var app = require('express')()
-var http = require('http').createServer(app)
-var io = require('socket.io')(http)
-let port = 3000
+var app = require("express")()
+var http = require("http").createServer(app)
+var io = require("socket.io")(http)
+var port = 3000;
 
-app.get('/', (req, res) =>{
-	res.sendFile( __dirname + '/index.html')
+app.get("/",(req, res)=>{
+	res.sendFile( __dirname + "/index.html")
 })
 
-io.on('connection', (socket) => {
-	console.log('s1')
-	socket.on('chat', (msg) => {
-		console.log('s2')
+io.on("connection", (socket)=>{
+	console.log("client connected")
+	socket.on("chat", (msg)=>{
+		console.log(msg)
 	})
 })
 
 http.listen(port, () =>{
-	console.log('server on *: ' + port)
+	console.log("listening at *: " + port)
 })
